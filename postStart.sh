65758f4d343eec0e20cbd60248b679e18abc65bb
Steve Boardwell <steve.boardwell@datameer.com>
2019-01-19 01:14:10 +0100

    fix(errors) general script improvements
    * made `postStart.sh` idempotent
       * now checks for scripts before applying
       * updating scripts if already there
    * made nexus scripts idempotent
       * repositories only created if they do not yet exist
       * same with maven-group

--- postStart.sh
+++ postStart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -eu
 HOST=localhost:8081
 
 until $(curl --output /dev/null --silent --head --fail http://$HOST/); do
@@ -14,23 +14,34 @@ find /nexus-data -type d -exec chmod g+x {} +
 USERNAME=admin
 PASSWORD=admin123
 
-if curl --fail --silent -u $USERNAME:$PASSWORD http://$HOST/service/metrics/ping
-then 
-    curl --fail -u $USERNAME:$PASSWORD --header "Content-Type: application/json" "http://$HOST/service/rest/v1/script/" -d @/opt/sonatype/nexus/admin_password.json
-    curl --fail -X POST -u $USERNAME:$PASSWORD --header "Content-Type: text/plain" "http://$HOST/service/rest/v1/script/admin_password/run"
+function createOrUpdateAndRun() {
+    local scriptName=$1
+    local scriptFile=$2
+    if [ "${#SCRIPT_LIST[@]}" = 0 ] || [[ ! " ${SCRIPT_LIST[@]} " =~ " ${scriptName} " ]]; then
+        echo "Creating $scriptName repository script"
+        curl --fail -X POST -u $USERNAME:$PASSWORD --header "Content-Type: application/json" "http://$HOST/service/rest/v1/script/" -d @$scriptFile
+    else
+        echo "Updating $scriptName repository script"
+        curl --fail -X PUT -u $USERNAME:$PASSWORD --header "Content-Type: application/json" "http://$HOST/service/rest/v1/script/$scriptName" -d @$scriptFile
+    fi
+    echo "Running $scriptName repository script"
+    curl --fail -X POST -u $USERNAME:$PASSWORD --header "Content-Type: text/plain" "http://$HOST/service/rest/v1/script/$scriptName/run"
+    echo
+}
+
+if curl --fail --silent -u $USERNAME:$PASSWORD http://$HOST/service/metrics/ping; then
+
+    # initialising the scripts already present once and assuming that there no duplicate script names in the scripts that follow
+    SCRIPT_LIST=($(curl --fail -s -u $USERNAME:$PASSWORD http://$HOST/service/rest/v1/script | grep -oE "\"name\" : \"[^\"]+" | sed 's/"name" : "//'))
+
+    createOrUpdateAndRun admin_password /opt/sonatype/nexus/admin_password.json
 fi
 
 PASSWORD=`cat /opt/sonatype/nexus/config/password`
 
 REPOS=($(ls /opt/sonatype/nexus/repositories | grep json | sed -e 's/\..*$//'))
-# we should check if we already have repos configured to avoid errors in logs when adding duplicates
-for i in "${REPOS[@]}"
-do
-    echo "\ncreating $i repository"
-    curl -u $USERNAME:$PASSWORD --header "Content-Type: application/json" "http://$HOST/service/rest/v1/script/" -d @/opt/sonatype/nexus/repositories/$i.json
-    curl -X POST -u $USERNAME:$PASSWORD --header "Content-Type: text/plain" "http://$HOST/service/rest/v1/script/$i/run"
+for i in "${REPOS[@]}"; do
+    createOrUpdateAndRun $i /opt/sonatype/nexus/repositories/$i.json
 done
 
-echo "\ncreating maven group"    
-curl -u $USERNAME:$PASSWORD --header "Content-Type: application/json" "http://$HOST/service/rest/v1/script/" -d @/opt/sonatype/nexus/maven-group.json
-curl -X POST -u $USERNAME:$PASSWORD --header "Content-Type: text/plain" "http://$HOST/service/rest/v1/script/maven-group/run"
+createOrUpdateAndRun maven-group /opt/sonatype/nexus/maven-group.json
