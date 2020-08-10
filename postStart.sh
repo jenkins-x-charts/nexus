f05c2172a550c6bf3daf2c25eef940e67290d346
James Strachan <james.strachan@gmail.com>
2020-08-10 11:06:04 +0100

    Revert "Revert "fix: Bump version and remove scripting usage in Nexus""
    This reverts commit f22067d9

--- postStart.sh
+++ postStart.sh
@@ -7,42 +7,38 @@ until curl --output /dev/null --silent --head --fail http://${HOST}/; do
   sleep 5
 done
 
+
 chgrp -R 0 /nexus-data
 chmod -R g+rw /nexus-data
 find /nexus-data -type d -exec chmod g+x {} +
 
 NEXUS_BASE_DIR="/opt/sonatype/nexus"
-NEXUS_REPO_DIR="${NEXUS_BASE_DIR}/repositories"
+NEXUS_MAVEN_REPO_DIR="${NEXUS_BASE_DIR}/maven-proxy-repositories"
+NEXUS_NPMJS_REPO_DIR="${NEXUS_BASE_DIR}/npmjs-proxy-repositories"
+NEXUS_MAVEN_GROUP_DIR="${NEXUS_BASE_DIR}/maven-group-repositories"
 USERNAME="admin"
 PASSWORD="admin123"
 PASSWORD_FROM_FILE="$(cat "${NEXUS_BASE_DIR}"/config/password || true)"
-declare -a SCRIPT_LIST
 
 function die() {
     echo "ERROR: $*" 1>&2
     exit 1
 }
 
-function createOrUpdateAndRun() {
-    local scriptFile scriptName scriptNameRegex
-    scriptFile="${1}.json"
-    scriptName="$(basename "${1}")"
-    scriptNameRegex=" ${scriptName} "
-    if [ "${#SCRIPT_LIST[@]}" = 0 ] || [[ ! " ${SCRIPT_LIST[*]} " =~ ${scriptNameRegex} ]]; then
-        echo "Creating ${scriptName} repository script"
-        curl --fail -X POST -u "${USERNAME}":"${PASSWORD}" --header "Content-Type: application/json" "http://${HOST}/service/rest/v1/script/" -d @"${scriptFile}"
-    else
-        echo "Updating ${scriptName} repository script"
-        curl --fail -X PUT -u "${USERNAME}":"${PASSWORD}" --header "Content-Type: application/json" "http://${HOST}/service/rest/v1/script/${scriptName}" -d @"${scriptFile}"
-    fi
-    echo "Running ${scriptName} repository script"
-    curl --fail -X POST -u "${USERNAME}":"${PASSWORD}" --header "Content-Type: text/plain" "http://${HOST}/service/rest/v1/script/${scriptName}/run"
-    echo
-}
+# Call the Nexus API for the specified repository
+function runCurlFromJsonFile() {
+    local jsonFile
+    local service
+    local body
+    local method
+    jsonFile="${1}"
+    service="${2}"
+    method="${3}"
+    body="$(cat "${jsonFile}")"
 
-function setScriptList() {
-    # initialising the scripts already present once and assuming that there no duplicate script names in the scripts that follow
-    mapfile -t SCRIPT_LIST < <(curl --fail -s -u "${USERNAME}":"${PASSWORD}" http://"${HOST}"/service/rest/v1/script | grep -oE "\"name\" : \"[^\"]+" | sed 's/"name" : "//')
+    if  ! curl --fail -X "${method}" -u "${USERNAME}":"${PASSWORD}" "http://${HOST}/${service}" -H "accept:application/json" -H "Content-Type:application/json" -d "${body//[$'\t\r\n']}"; then
+        echo "Error calling Nexus API for http://${HOST}/${service} to add repository from ${jsonFile}"
+    fi
 }
 
 function setPasswordFromFile() {
@@ -56,15 +52,13 @@ function setPasswordFromFile() {
 
 if curl --fail --silent -u "${USERNAME}":"${PASSWORD}" http://"${HOST}"/service/metrics/ping; then
     echo "Login to nexus succeeded. Default password worked. Updating password if available..."
-    setScriptList
-    createOrUpdateAndRun "${NEXUS_BASE_DIR}"/admin_password
+    curl --fail -X PUT -u "${USERNAME}":"${PASSWORD}" "http://${HOST}/service/rest/beta/security/users/admin/change-password" --header "Content-Type: text/plain" -d "${PASSWORD_FROM_FILE}"
     setPasswordFromFile
 elif [ -n "${PASSWORD_FROM_FILE}" ]; then
     setPasswordFromFile
     echo "Default password failed. Checking password file..."
     if curl --fail --silent -u "${USERNAME}":"${PASSWORD}" http://"${HOST}"/service/metrics/ping; then
         echo "Login to nexus succeeded. Password from secret file worked."
-        setScriptList
     else
         die "Login to nexus failed. Tried both the default password and the provided password secret file."
     fi
@@ -72,14 +66,30 @@ else
     die "Login to nexus failed. Tried the default password only since no password secret file was provided."
 fi
 
-
-mapfile -t REPOS < <(find "${NEXUS_REPO_DIR}" -maxdepth 1 -type f -name "*json*" | sed -e 's/\..*$//')
+# For each maven repository proxy json file, create the repo proxy via the Nexus API.
+echo "Creating maven proxy repositories from json"
+mapfile -t REPOS < <(find "${NEXUS_MAVEN_REPO_DIR}" -maxdepth 1 -type f -name "*json*")
 for repo in "${REPOS[@]}"; do
-    createOrUpdateAndRun "${repo}"
+    runCurlFromJsonFile "${repo}" "service/rest/beta/repositories/maven/proxy" POST
 done
 
-createOrUpdateAndRun "${NEXUS_BASE_DIR}"/maven-group
+# For each npmjs proxy repository json file, create the repo proxy via the Nexus API.
+echo "Creating npmjs proxy repositories from json"
+mapfile -t REPOS < <(find "${NEXUS_NPMJS_REPO_DIR}" -maxdepth 1 -type f -name "*json*")
+for repo in "${REPOS[@]}"; do
+    runCurlFromJsonFile "${repo}" "service/rest/beta/repositories/npm/proxy" POST
+done
 
-if [ -z "${ENABLE_ANONYMOUS_ACCESS}" ]; then
-  createOrUpdateAndRun "${NEXUS_BASE_DIR}"/disable-anonymous-access
+# For each maven group repository json file, create the repo via the Nexus API.
+echo "Creating maven group repositories from json"
+mapfile -t REPOS < <(find "${NEXUS_MAVEN_GROUP_DIR}" -maxdepth 1 -type f -name "*json*")
+for repo in "${REPOS[@]}"; do
+    runCurlFromJsonFile "${repo}" "service/rest/beta/repositories/maven/group" POST
+done
+
+# It is not possible at this time to disable anonymous access at the server level via API.
+# By disabling the anonymous user account, it has the same result.
+if [ -z "${ENABLE_ANONYMOUS_ACCESS}" ] || [ "${ENABLE_ANONYMOUS_ACCESS}" = "false" ]; then
+    echo "Disabling the anonymous account"
+    runCurlFromJsonFile "${NEXUS_BASE_DIR}"/disable-anonymous-access.json "service/rest/beta/security/users/anonymous" PUT
 fi
